2e63176126e33a122af3d096004388896b76ba2c
HassamAli2174 <hassamarshad021@gmail.com>
2025-09-20 16:36:34 +0500

    Add interactive components to home page with enhanced styling and functionality more over added custom pipe for search items on the TODO screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,45 @@
 import { Component, signal } from '@angular/core';
+import { Router } from '@angular/router';
 import { GreetingComponent } from "../components/greeting/greeting.component";
 import { CounterComponent } from '../components/counter/counter.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [GreetingComponent, CounterComponent],
+  imports: [GreetingComponent, CounterComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-homeMessage = signal('message coming from home component');
+  homeMessage = signal('Welcome to our amazing Angular application! 🚀');
+  lastKeyPressed = signal<string>('');
 
-keyUpHandler(event: KeyboardEvent){
-console.log(`user pressed the ${event.key} key`);
-}
+  constructor(private router: Router) {}
+
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+    console.log('Event target:', event.target);
+    this.lastKeyPressed.set(event.key);
+    
+    // Clear the last key after 3 seconds
+    setTimeout(() => {
+      this.lastKeyPressed.set('');
+    }, 3000);
+  }
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    console.log('Input value:', target.value);
+  }
+
+  navigateToTodos() {
+    this.router.navigate(['/todos']);
+  }
+
+  refreshData() {
+    // Clear the live input data
+    this.lastKeyPressed.set('');
+    console.log('Refreshing data...');
+    // You can add actual refresh logic here
+  }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -7,30 +7,6 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TodosService {
   http = inject(HttpClient);
-  todoItems: Array<Todo> = [
-    {
-      title: 'Buy groceries',
-      userId:1,
-      id: 0,
-      completed: false
-    },
-    
-    {
-      title: 'car wash',
-      userId:2,
-      id: 1,
-      completed: false
-    },
-    
-    {
-      title: 'washing',
-      userId:3,
-      id: 1,
-      completed: false
-    },
-    
-  ]
-  set: any;
 
   getTodoFromApi() {
     const url  = `https://jsonplaceholder.typicode.com/todos/`
